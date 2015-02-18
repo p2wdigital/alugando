c645795562c31841adeed4a0a526f5a3e80f8e47
Paulo Matos <palex.oliv@icosites.com.br>
2015-02-18 20:02:11 -0300

    Update

--- src/Admin/Controller/MenuController.php
+++ src/Admin/Controller/MenuController.php
@@ -4,10 +4,11 @@ namespace Admin\Controller;
 
 use Plunder\Core\Controller\Controller;
 use Plunder\Core\HttpRequest\Request;
+use Plunder\Core\HttpRequest\Response;
 use Table\Model\PostQuery;
 
 /**
-* @Route("/admin/menu") 
+* @Route("/admin/menu", name="menu_home") 
 */
 class MenuController extends Controller
 {
@@ -21,9 +22,22 @@ class MenuController extends Controller
 	}
 
 	/**
-	 * @Route("/insert", name="admin_insert")
+	 * @Route("/teste", name="menu_teste")
 	 */
-	public function insertAction(Request $request){
+	public function insertAction(){
+		$arr = array(
+			array('nome'=>'paulo', 'marca'=>'safra', "parent"=>array(
+													'nome'=>'paulinho', 'marca'=>'jsafra'
+				)),
+			array('nome'=>'paulo2', 'marca'=>'safra2', "parent"=>array(
+												array('nome'=>'paulinho2', 'marca'=>'jsafra2'),
+												array("nome"=>'mais ', "marca"=>"outra"),
+				)
+			),
+		);
+		return new Response(print_r(json_decode($_POST['data']), true));
+		//return new Response(print_r(json_encode($arr), true));
+
 	}
 
 	/**
